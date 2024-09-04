61b540c89c370ebe57528f02ac321b75086bc01e
Biruh Tesfaye <biruhtesfaye2121@gmail.com>
2024-09-04 15:56:03 +0300

    write the spider for exhibitors

---
+++ exhibitors/exhibitors/spiders/crawler.py
@@ -0,0 +1,28 @@
+from typing import Iterable
+from urllib.parse import urljoin
+import scrapy
+
+
+class CrawlerSpider(scrapy.Spider):
+    name = "crawler"
+    allowed_domains = ["lasvegasaug.fashionresource.com"]
+
+    start_urls = ["https://lasvegasaug.fashionresource.com/newfront/marketplace/exhibitors?pageNumber=1"]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.parse)
+
+    def parse(self, response):
+        with open("exhibitors.html", "w") as f:
+            f.write(response.text)
+            
+        list_links = response.xpath('(//div[contains(@class, "MuiGrid-item")]//a[contains(@class, "MuiTypography-root")])[position()>1]/@href').getall()
+        for link in list_links:
+            yield {scrapy.Request(urljoin("https://lasvegasaug.fashionresource.com", link), callback=self.parse_detail)}
+
+    def parse_detail(self, response):
+        yield {
+            "name": response.xpath('//h2/text()').get(),
+            "website": response.xpath('//h6/text()').get(),
+        }
